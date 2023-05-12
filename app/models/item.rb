33aa9131f0113b2724fd351234dace3a005e69ab
miroslav <miroslav@kolosek.com>
2023-05-12 11:46:30 +0200

    Gem Rubocop

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,9 @@ class Item < ApplicationRecord
       "Item name is empty."
     end
   end
+
+  def check
+    is_rails_testing_env = Rails.env.test? ? true : false
+    puts "This is just one long line to make Rubocop fail. Nothing Else" if is_rails_testing_env && Item.count > 1 && Item.count < 10
+  end
 end
